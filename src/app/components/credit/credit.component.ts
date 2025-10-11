f9626a3741daeb7cffa0791204fcb0d6f1d7bf97
Iris <wirisguedes@gmail.com>
2025-10-11 13:20:07 -0300

    components

---
+++ src/app/components/credit/credit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit',
+  standalone: true,
+  imports: [],
+  templateUrl: './credit.component.html',
+  styleUrl: './credit.component.scss'
+})
+export class CreditComponent {
+
+}
---
+++ src/app/components/debit/debit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debit',
+  standalone: true,
+  imports: [],
+  templateUrl: './debit.component.html',
+  styleUrl: './debit.component.scss'
+})
+export class DebitComponent {
+
+}
